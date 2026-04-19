1a3548f1f1bebea66eda2581f686323baabd9575
Felippe Santana <felippemenezes1982@gmail.com>
2026-04-19 20:47:29 -0300

    Iterate with array

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,32 +1,29 @@
 def stock_picker(array)
-  hash = Hash.new(0)
-  array.each_with_index do | value, index |
-    hash[index] = value
-  end
+  lower_value = array[0]
+  buy_day = 0
 
   higher_value = 0
   sell_day = 0
 
-  hash.each do | key, value |
-    if value > higher_value && key > sell_day
-      higher_value = value
-      sell_day = key
-    end
-  end
+  max_profit = 0
 
-  lower_value = array[0]
-  buy_day = ""
-
-  hash.each do | key, value |
-    if key < sell_day && value < lower_value
+  array.each_with_index do |value, index|
+    if value < lower_value
       lower_value = value
-      buy_day = key
+      buy_day = index
+    end
+
+    profit = value - lower_value
+
+    if profit > max_profit
+      max_profit = profit
+      higher_value = value
+      sell_day = index
     end
   end
 
-  result = [ buy_day, sell_day]
-  p "for a profit of $#{higher_value} - $#{lower_value} == $#{higher_value - lower_value}"
-  result
+  p "for a profit of $#{higher_value} - $#{lower_value} == $#{max_profit}"
+  [buy_day, sell_day]
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
